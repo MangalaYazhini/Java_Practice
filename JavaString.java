caa29f9a84620a904b5d9a31167eec11c0a3d70e
Mangala Yazhini E S <166918891+MangalaYazhini@users.noreply.github.com>
2026-01-27 13:02:22 +0530

    Java Strings

---
+++ JavaString.java
@@ -0,0 +1,31 @@
+
+public class JavaString {
+  
+  public static void main(String[] args) {
+    
+
+    String mystr1 = "java"; 
+    String mystr2 = "java"; 
+
+    String mystr3 = new String("java");
+ 
+    if(mystr1 == mystr2)
+    {
+      System.out.println("mystr1 and mystr2 are referred to the same string");
+    }
+    if(mystr1 != mystr3)
+    {
+      System.out.println("\nmystr1 and mystr3 are not referring the same string");
+      System.out.println("mystr1 -> SCP");
+      System.out.println("mystr3 -> String object in heap");
+    }
+    if(mystr2 != mystr3)
+    {
+      System.out.println("\nmystr2 and mystr3 are not referring the same string");
+      System.out.println("mystr1 -> SCP");
+      System.out.println("mystr3 -> String object in heap");
+    }
+  }
+  
+
+}
